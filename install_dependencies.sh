9c173193169a236f4831e850e5e2951409731695
acesti84 <andrea@acesti.it>
2020-03-21 09:43:58 +0100

    added title and fixed non utf8 issues

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 pip install twython
-sudo apt-get install python-setuptools
+
+sudo apt-get install python-setuptools \
+python3-lxml \
+python-lxml
+
+export PYTHONIOENCODING=utf8
+
 exit 0
--- wikiRandomPage.py
+++ wikiRandomPage.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 import sys
 import time 
@@ -5,19 +6,28 @@ import socket
 import requests
 from myconf import *
 from datetime import datetime
+from twython import Twython
+from lxml.html import fromstring
 
+# get time
 now = datetime.now()
-
 current_hour = now.strftime("%H")
 
-from twython import Twython
-
+# get url
 r = requests.get('https://it.wikipedia.org/wiki/Speciale:PaginaCasuale') 
-print(r.url) 
- 
-tweetStr = "La pagina delle " +  current_hour + ": " + r.url + "\n#wiki #random #wikipedia #wikimedia"
+
+# get title
+title = fromstring(r.content).findtext('.//title')
+
+# remove "- Wikipedia"
+title = title [:-11]
+
+# string creation
+tweetStr = "La pagina delle " +  current_hour + ": " + title + "\n\n" + r.url + "\n\n#wiki #random #wikipedia #wikimedia"
+
+# tweet sending
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_SECRET)
 api.update_status(status=tweetStr)
 
-print "Tweeted: " + tweetStr
+print "\n" + tweetStr
 
